9a4ed0a4909bee4ff81192ac14ab0d8b09d65008
joeljhou <joeljhou336@gmail.com>
2025-06-10 01:10:29 +0800

    初始化一个java web工程

---
+++ src/main/java/com/geekyspace/controller/ExampleController.java
@@ -0,0 +1,10 @@
+package com.geekyspace.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+    @GetMapping("/")
+    public String home() {return "success";}
+}
